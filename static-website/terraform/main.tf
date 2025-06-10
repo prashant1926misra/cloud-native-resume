a9952b6ee6e3129d2a5238f4bb3a0da4465e94b0
prashant1926misra <prashant2611misra@gmail.com>
2025-06-10 11:26:30 +0000

    phase-1 | terraform main.tf file created

---
+++ static-website/terraform/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    Environment = "MVP"
+    Project     = "cloud-native-resume"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "PublicReadGetObject",
+        Effect: "Allow",
+        Principal: "*",
+        Action: "s3:GetObject",
+        Resource: "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
