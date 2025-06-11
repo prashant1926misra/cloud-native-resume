07add6abba99063af85e767a2986573ad26859fc
prashant1926misra <prashant2611misra@gmail.com>
2025-06-11 03:44:00 +0000

    phase-1 | main.tf updated | replace ACL with bucket policy for public access

--- static-website/terraform/main.tf
+++ static-website/terraform/main.tf
@@ -3,12 +3,19 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
-  acl    = "public-read"
+  bucket = var.bucket_nam
+  #acl    = "public-read"
 
   website {
     index_document = "index.html"
   }
+
+  ownership_controls {
+     rule {
+       object_ownership = "BucketOwnerEnforced"
+     }
+   }
+
 
   tags = {
     Environment = "MVP"
@@ -18,6 +25,11 @@ resource "aws_s3_bucket" "static_site" {
 
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = aws_s3_bucket.static_site.id
+  block_public_acls = false
+  ignore_public_acls  = false
+  block_public_policy =  false
+  restrict_public_buckets = false 
+
 
   policy = jsonencode({
     Version = "2012-10-17",
