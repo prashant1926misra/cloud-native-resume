8f1b69a6d55a4eef9a988ff9655d742089753272
prashant1926misra <prashant2611misra@gmail.com>
2025-07-12 16:53:51 +0000

    Add IAM policy for logging and SES access

--- contact-form/terraform/main.tf
+++ contact-form/terraform/main.tf
@@ -21,3 +21,31 @@ resource "aws_iam_role" "lambda_exec_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+# IAM Policy attachment for Lambda logging and SES access
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
