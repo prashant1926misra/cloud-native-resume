0de88cd7e97b9cda3c2fb2086836619629c09e2d
prashant1926misra <prashant2611misra@gmail.com>
2025-07-13 17:29:55 +0000

    Add API Gateway v2 integration with Lambda for /contact endpoint

--- contact-form/terraform/main.tf
+++ contact-form/terraform/main.tf
@@ -73,3 +73,51 @@ resource "aws_lambda_function" "lambda_handler" {
     }
   }
 }
+
+
+#API Gateway Lambda integration 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_route
+#https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-routes.html
+
+# Create api gateway 
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "ContactFormAPI"
+  protocol_type = "HTTP"
+}
+
+# API Gateway Lambda Integration
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.api_gateway.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.lambda_handler.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+# API Gateway Stage
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# API Gateway Route
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /contact"    # HTTP method/resource path   
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+
+# Lambda permission for API Gateway
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.contact_form_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
+
+
+
+
