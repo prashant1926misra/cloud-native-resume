680a807adaf5db3c8a26a783bc82c672d4c659b4
prashant1926misra <prashantnau2611@gmail.com>
2025-07-20 19:57:13 +0530

    Add Lambda function to increment and return visitor count

---
+++ visitor-counter/lambda/main.py
@@ -0,0 +1,39 @@
+'''
+Documentation:
+https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html
+https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/python/example_code/dynamodb#code-examples
+https://docs.aws.amazon.com/code-library/latest/ug/python_3_dynamodb_code_examples.html
+
+'''
+
+import boto3
+import json
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('visitor_count_table')
+
+    try:
+        response = table.update_item(
+            Key={'id': 'visits'},
+            UpdateExpression='ADD #count :incr',
+            ExpressionAttributeNames={'#count': 'count'},
+            ExpressionAttributeValues={':incr': 1},
+            ReturnValues='UPDATED_NEW'
+        )
+
+        new_count = response['Attributes']['count']
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': json.dumps({'count': new_count})
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
