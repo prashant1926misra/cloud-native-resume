61a9ea19995e14d08f290617cf0ebf9533f073f2
prashant1926misra <prashant2611misra@gmail.com>
2025-07-30 03:27:39 +0000

    Add iam role and policy for codepipeline in main.tf

--- ci-cd/terraform/main.tf
+++ ci-cd/terraform/main.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region = "ap-south-1" 
 }
 
-# ***********IAM Role for CodeBuild***********
+# --------------------------------------
+# IAM Role for CodeBuild
+# --------------------------------------
 
 # This role allows CodeBuild to access AWS resources
 resource "aws_iam_role" "codebuild_role" {
@@ -38,6 +40,48 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       {
         Effect   = "Allow"
         Action   = ["logs:*"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+# --------------------------------------
+# IAM Role for CodePipeline
+# --------------------------------------
+
+# This role allows CodePipeline to trigger CodeBuild and access AWS services
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Policy attached to the role: allows it to start CodeBuild & access S3
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline-policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "codebuild:*",
+          "s3:*",
+          "iam:PassRole"
+        ]
         Resource = "*"
       }
     ]
